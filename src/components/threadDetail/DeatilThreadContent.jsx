da8ff1300aab8a6a83ea08cc851fa1832e824fc2
KusAndiPriyono <andipriyono94@gmail.com>
2023-07-08 19:21:48 +0700

    .

---
+++ src/components/threadDetail/DeatilThreadContent.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable react/react-in-jsx-scope */
+import { Box } from '@mui/material';
+import parse from 'html-react-parser';
+import PropTypes from 'prop-types';
+import { detailProp } from '../../utils/propHelper';
+
+function DetailThreadContent({ detail }) {
+  const { body } = detail;
+
+  return <Box sx={{ m: 2 }}>{parse(body)}</Box>;
+}
+
+DetailThreadContent.propTypes = {
+  detail: PropTypes.shape(detailProp).isRequired,
+};
+
+export default DetailThreadContent;

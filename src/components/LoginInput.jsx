22661350e22c6f8114d5b905294b825cfd78150c
KusAndiPriyono <andipriyono94@gmail.com>
2023-07-08 19:23:16 +0700

    .

---
+++ src/components/LoginInput.jsx
@@ -0,0 +1,117 @@
+/* eslint-disable react/jsx-curly-brace-presence */
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable import/order */
+/* eslint-disable prettier/prettier */
+import {
+  Avatar,
+  Box,
+  Button,
+  Grid,
+  TextField,
+  Paper,
+  Typography,
+} from '@mui/material';
+import useInput from '../hooks/useInput';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+function Copyright(props) {
+  return (
+    <Typography
+      variant='body2'
+      color='text.secondary'
+      align='center'
+      {...props}
+    >
+      {'Copyright © '}
+      <Link color='inherit' href='https://mui.com/'>
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+function LoginInput({ login }) {
+  const [email, onEmailChange] = useInput('');
+  const [password, onPasswordChange] = useInput('');
+
+  const handleLogin = () => {
+    login({ email, password });
+  };
+
+  return (
+    <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+      <Box
+        sx={{
+          my: 8,
+          mx: 4,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component='h1' variant='h5'>
+          Login
+        </Typography>
+        <Box component='form' sx={{ mt: 1 }}>
+          <TextField
+            margin='normal'
+            required
+            fullWidth
+            label='Email Address'
+            name='email'
+            autoComplete='email'
+            autoFocus
+            type='email'
+            value={email}
+            onChange={onEmailChange}
+          />
+          <TextField
+            margin='normal'
+            required
+            fullWidth
+            name='password'
+            label='Password'
+            type='password'
+            autoComplete='current-password'
+            value={password}
+            onChange={onPasswordChange}
+          />
+
+          <Button
+            type='button'
+            fullWidth
+            variant='contained'
+            sx={{ mt: 3, mb: 2 }}
+            onClick={handleLogin}
+          >
+            Login
+          </Button>
+
+          <Grid container>
+            <Grid item>
+              <Link to='/register' variant='body2'>
+                Don't have an account? Register
+              </Link>
+            </Grid>
+          </Grid>
+          <Copyright sx={{ mt: 5 }} />
+        </Box>
+      </Box>
+    </Grid>
+  );
+}
+
+LoginInput.propTypes = {
+  login: PropTypes.func.isRequired,
+};
+
+export default LoginInput;

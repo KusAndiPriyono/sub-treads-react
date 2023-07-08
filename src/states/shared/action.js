c60ed778bd8501854f0bf6a1a544096feca9e921
KusAndiPriyono <andipriyono94@gmail.com>
2023-07-08 19:12:54 +0700

    .

---
+++ src/states/shared/action.js
@@ -0,0 +1,27 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable no-alert */
+/* eslint-disable prettier/prettier */
+import { hideLoading, showLoading } from 'react-redux-loading-bar';
+import api from '../../utils/api';
+import { receiveThreadsActionCreator } from '../thread/action';
+import { receiveUsersActionCreator } from '../users/action';
+
+function asyncPopulateUsersAndThreads() {
+  return async (dispatch) => {
+    dispatch(showLoading());
+    try {
+      const users = await api.getAllUsers();
+      const threads = await api.getAllThreads();
+
+      dispatch(receiveUsersActionCreator(users));
+      dispatch(receiveThreadsActionCreator(threads));
+    } catch (error) {
+      alert(error.message);
+    }
+    dispatch(hideLoading());
+  };
+}
+
+export { asyncPopulateUsersAndThreads };

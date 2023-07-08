35b81b2ffa7d55f531d3e19a66d150fd7773a796
KusAndiPriyono <andipriyono94@gmail.com>
2023-07-08 19:18:23 +0700

    .

---
+++ src/hooks/useInput.js
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+import { useState } from 'react';
+
+function useInput(defaultValue = '') {
+  const [value, setValue] = useState(defaultValue);
+
+  function handleValueChange({ target }) {
+    try {
+      setValue(target.value);
+    } catch (error) {
+      setValue('');
+    }
+  }
+
+  return [value, handleValueChange, setValue];
+}
+
+export default useInput;

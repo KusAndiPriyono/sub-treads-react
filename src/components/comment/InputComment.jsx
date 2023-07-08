59b613782b070a867989fda27948fb95feb510c9
KusAndiPriyono <andipriyono94@gmail.com>
2023-07-08 19:19:27 +0700

    .

---
+++ src/components/comment/InputComment.jsx
@@ -0,0 +1,43 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable import/order */
+/* eslint-disable prettier/prettier */
+import { useDispatch } from 'react-redux';
+import { asyncAddComment } from '../../states/threadDetail/action';
+import { Button, TextField } from '@mui/material';
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+
+function InputComment({ threadId }) {
+  const [content, setValue] = useState('');
+  const dispatch = useDispatch();
+
+  const onChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const onAddComment = () => {
+    dispatch(asyncAddComment(threadId, content));
+  };
+
+  return (
+    <form>
+      <TextField
+        onInput={onChange}
+        multiline
+        rows={4}
+        value={content}
+        sx={{ width: '100%' }}
+      />
+      <Button sx={{ m: 2 }} variant='contained' onClick={() => onAddComment()}>
+        Send
+      </Button>
+    </form>
+  );
+}
+
+InputComment.propTypes = {
+  threadId: PropTypes.string.isRequired,
+};
+
+export default InputComment;
